aeec0c0b2cd9a96cf4f330fc3bc49528b02bd57d
Alex Kim <kim_alex@mail.ru>
2020-03-20 11:44:28 +0300

    add some style Authentication2

--- src/Boxes/Authentication.js
+++ src/Boxes/Authentication.js
@@ -20,23 +20,27 @@ export default function(props){
 	return (<div className="container-form">
 		<div className="title-form">Sing in</div>
 		<form className="registration-form" onSubmit={handleSubmit}>
-			<input 
-				className="registration-form__input"
-				type="text" 
-				name="login"
-				value={inputData.login} 
-				onChange={handleInputChange}
-				placeholder="login"></input>
-			
-			<input 
-				className="registration-form__input"
-				type="text" 
-				name="password"
-				value={inputData.password} 
-				onChange={handleInputChange}
-				placeholder="password"></input>
+			<div className="input-wrapper">
+				<input 
+					className="registration-form__input"
+					type="text" 
+					name="login"
+					value={inputData.login} 
+					onChange={handleInputChange}
+					placeholder="Email Address *"></input>
 
-			<div>
+			</div>
+			<div className="input-wrapper">
+			
+				<input 
+					className="registration-form__input"
+					type="text" 
+					name="password"
+					value={inputData.password} 
+					onChange={handleInputChange}
+					placeholder="Password *"></input>
+			</div>
+			<div className="input-wrapper">
 				<input
 					type="checkbox"
 					name="memento"
@@ -44,11 +48,12 @@ export default function(props){
 					onChange={handleInputChange}></input>
 				<span>Remember me</span>
 			</div>
-
-			<input 
-				className="registration-form__input registration-form__input--submit"
-				type="submit" 
-				value="Sing in"></input>
+			<div className="input-wrapper">
+				<input 
+					className="registration-form__input registration-form__input--submit"
+					type="submit" 
+					value="Sing in"></input>
+			</div>
 		</form>
 		<div className="container-links">
 			<a className="link" href="#">Forgot password?</a>
