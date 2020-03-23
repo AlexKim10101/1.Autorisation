3c88c4fb36e3e0b29a7c9c7b37b714fc7fbc9b77
Alex Kim <kim_alex@mail.ru>
2020-03-23 08:49:32 +0300

    added material ui elements in authentication

--- src/Boxes/Authentication.js
+++ src/Boxes/Authentication.js
@@ -2,9 +2,20 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux'
 import {mapStateToProps, mapDispatchToProps} from '../Actions';
 
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+
 function Authentication(props){
 
-	const [inputData, setInputData] = useState({login:'', password:'', memento: false});
+	const [inputData, setInputData] = useState({email:'', password:'', memento: false});
 
 	function handleInputChange(event){
 		const target = event.target;
@@ -19,49 +30,72 @@ function Authentication(props){
 		console.log(inputData);
 	}
 
-	return (<div className="container-form">
-		<div className="title-form">Sing in</div>
-		<form className="registration-form" onSubmit={handleSubmit}>
-			<div className="input-wrapper">
-				<input 
-					className="registration-form__input"
-					type="text" 
-					name="login"
-					value={inputData.login} 
-					onChange={handleInputChange}
-					placeholder="Email Address *"></input>
+	return (
+		<div className="container-form">
+			<div className="title-form">Sing in</div>
+			<form className="registration-form" onSubmit={handleSubmit}>
+				<div className="input-wrapper">
+					<TextField
+			            variant="outlined"
+			            margin="normal"
+			            required
+			            fullWidth
+			            id="email"
+			            label="Email Address"
+			            name="email"
+			            autoComplete="email"
+			            autoFocus
+			            value={inputData.login} 
+						onChange={handleInputChange}
+			          />					
+				</div>
+				<div className="input-wrapper">
+					<TextField
+			            variant="outlined"
+			            margin="normal"
+			            required
+			            fullWidth
+			            name="password"
+			            label="Password"
+			            type="password"
+			            id="password"
+			            autoComplete="current-password"
+			            value={inputData.password} 
+						onChange={handleInputChange}
+			          />
+					
+				</div>
+				<div className="input-wrapper">
+					<FormControlLabel
+			            control={<Checkbox 
+			            	color="primary" 
+			            	name ="memento" 
+			            	value={inputData.memento} 
+			            	onChange={handleInputChange}/>}
+			            label="Remember me"
+			            
+						
+			          />
+				</div>
+				<div className="input-wrapper">
 
+					<Button
+			            type="submit"
+			            fullWidth
+			            variant="contained"
+			            color="primary"
+			            
+			        >
+			        Sign In
+			        </Button>
+					
+				</div>
+			</form>
+			<div className="container-links">
+				<a className="link" href="#">Forgot password?</a>
+				<a className="link" href="#">Don't have an account? Sign Up</a>
 			</div>
-			<div className="input-wrapper">
-			
-				<input 
-					className="registration-form__input"
-					type="text" 
-					name="password"
-					value={inputData.password} 
-					onChange={handleInputChange}
-					placeholder="Password *"></input>
-			</div>
-			<div className="input-wrapper">
-				<input
-					type="checkbox"
-					name="memento"
-					value={inputData.memento}
-					onChange={handleInputChange}></input>
-				<span>Remember me</span>
-			</div>
-			<div className="input-wrapper">
-				<input 
-					className="registration-form__input registration-form__input--submit"
-					type="submit" 
-					value="Sing in"></input>
-			</div>
-		</form>
-		<div className="container-links">
-			<a className="link" href="#">Forgot password?</a>
-			<a className="link" href="#">Don't have an account? Sign Up</a>
-		</div>
-	</div>)
+		</div>)
 	
 	
 }
